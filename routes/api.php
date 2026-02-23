49d74f8f8b0c032422fc22ae74ff2cb6c7cb6d3c
muxammedqudaynazarov <m.qudaynazarov@gmail.com>
2026-02-23 16:29:18 +0500

    OK-75

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,15 @@
 
 use App\Http\Controllers\CurriculumController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\ResultController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/tests/autoFinishExams', [Resultcontroller::class, 'autoFinishExams']);
 
 Route::get('/get-specialties', [CurriculumController::class, 'getSpecialties'])->name('api.get-specialties');
 Route::post('/getsms/send', [LevelController::class, 'sms_sender']);
