cf6dc049f29ced483e7c5801f51c353a636f405c
muxammedqudaynazarov <m.qudaynazarov@gmail.com>
2026-02-23 17:14:35 +0500

    OK-75

--- app/Http/Controllers/LogViewController.php
+++ app/Http/Controllers/LogViewController.php
@@ -7,9 +7,22 @@ use Spatie\Activitylog\Models\Activity;
 
 class LogViewController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $logs = Activity::with('causer')->latest()->paginate(50);
+        $query = Activity::with('causer')->latest();
+
+        // Qidiruv mantiqi
+        if ($request->filled('search')) {
+            $search = $request->search;
+            // Polymorphic munosabat ichidan qidirish
+            $query->whereHasMorph('causer', '*', function ($q) use ($search) {
+                // Sizning bazangizda name JSON bo'lgani uchun LIKE bilan qidiramiz
+                $q->where('name', 'LIKE', '%' . $search . '%');
+            });
+        }
+
+        $logs = $query->paginate(50)->appends($request->query());
+
         return view('pages.web.logs.index', compact('logs'));
     }
 }
