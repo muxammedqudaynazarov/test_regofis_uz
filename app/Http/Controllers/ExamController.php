3fcf3a68acb81dbc42229730cbca143128dcfc18
muxammedqudaynazarov <m.qudaynazarov@gmail.com>
2026-02-26 15:26:00 +0500

    OK-81

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -85,7 +85,6 @@ class ExamController extends Controller
     public function show(Request $request, $id)
     {
         $exam = Exam::findOrFail($id);
-        dd($exam);
         if ($exam->finished == '1' && $exam->archived == '0') {
             $res = Result::where('exam_id', $exam->id)->firstOrFail();
             if ($res->status == '1' && $res->uploaded == '0') {
