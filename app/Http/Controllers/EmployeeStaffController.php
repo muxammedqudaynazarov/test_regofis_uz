432e39181010561a3d928bf4c1a285ee9ce652b4
muxammedqudaynazarov <m.qudaynazarov@gmail.com>
2026-05-15 16:39:25 +0500

    L-1

--- app/Http/Controllers/EmployeeStaffController.php
+++ app/Http/Controllers/EmployeeStaffController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Result;
 use Illuminate\Http\Request;
 
 class EmployeeStaffController extends Controller
 {
-    //
+    public function index()
+    {
+        $res = Result::whereNull('retrain_id')->get();
+        dd($res);
+    }
 }
