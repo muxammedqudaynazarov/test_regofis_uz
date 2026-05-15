184ccd62bd5e66bb4c6f123bb88c43970237c87e
muxammedqudaynazarov <m.qudaynazarov@gmail.com>
2026-05-15 16:49:28 +0500

    L-1

--- app/Models/Result.php
+++ app/Models/Result.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Traits\LogsTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Result extends Model
 {
@@ -19,4 +20,9 @@ class Result extends Model
         'user_id',
         'status',
     ];
+
+    public function exam(): hasOne
+    {
+        return $this->hasOne(Exam::class, 'id', 'exam_id');
+    }
 }
